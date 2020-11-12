3c0beaa21a878a33c5d737f01e5bb0fc35266f04
Pedro A.Moreno <pedabraham@gmail.com>
2020-11-12 14:40:30 -0600

    The react logo was replaced with a header

--- src/App.js
+++ src/App.js
@@ -267,7 +267,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="title">Guess the word</div>
+        {/*<img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -278,7 +279,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a>*/}
         <Game />
       </header>
 
